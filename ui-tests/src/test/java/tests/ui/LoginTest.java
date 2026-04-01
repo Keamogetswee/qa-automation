37faf12ba1f5eecab2b3bf63d5ec25ec8cc6d5e1
Kea Moseki <neoentlemoseki@gmail.com>
2026-04-01 17:29:23 +0200

    Add explicit wait to handle dynamic loading and prevent flaky tests

--- ui-tests/src/test/java/tests/ui/LoginTest.java
+++ ui-tests/src/test/java/tests/ui/LoginTest.java
@@ -3,8 +3,12 @@ package test.java.tests.ui;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginTest {
 
@@ -20,8 +24,10 @@ public class LoginTest {
         // 3. Click Signup/Login
         driver.findElement(By.linkText("Signup / Login")).click();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         // Get actual text
-        String actualText = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/h2")).getText();
+        String actualText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/h2"))).getText();
 
         // Expected text
         String expectedText = "Login to your account";
