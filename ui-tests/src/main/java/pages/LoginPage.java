8bbc2391103b293e23889603ccd62cdb31e6b5f4
Kea Moseki <neoentlemoseki@gmail.com>
2026-04-01 17:37:54 +0200

    Move explicit wait logic into POM for better stability and cleaner tests.

--- ui-tests/src/main/java/pages/LoginPage.java
+++ ui-tests/src/main/java/pages/LoginPage.java
@@ -1,21 +1,34 @@
+package main.java.pages;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class LoginPage {
 
     WebDriver driver;
+    WebDriverWait wait;
 
-    By emailField = By.name("email");
-    By passwordField = By.name("password");
-    By loginBtn = By.xpath("//button[text()='Login']");
-
+    // Constructor
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    public void login(String email, String password) {
-        driver.findElement(emailField).sendKeys(email);
-        driver.findElement(passwordField).sendKeys(password);
-        driver.findElement(loginBtn).click();
+    // Locators
+    By loginLink = By.linkText("Signup / Login");
+    By loginHeader = By.xpath("//h2[text()='Login to your account']");
+
+    // Actions
+    public void clickLogin() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginLink)).click();
+    }
+
+    public String getLoginHeaderText() {
+        return wait.until(
+                ExpectedConditions.visibilityOfElementLocated(loginHeader)
+        ).getText();
     }
 }
--- ui-tests/src/test/java/tests/ui/LoginTest.java
+++ ui-tests/src/test/java/tests/ui/LoginTest.java
@@ -1,39 +1,28 @@
 package test.java.tests.ui;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
+import main.java.pages.LoginPage;
 
 public class LoginTest {
 
     @Test
     public void openLoginPage() {
 
-        // 1. Launch browser
         WebDriver driver = new ChromeDriver();
 
-        // 2. Open website
         driver.get("https://automationexercise.com/");
 
-        // 3. Click Signup/Login
-        driver.findElement(By.linkText("Signup / Login")).click();
+        LoginPage loginPage = new LoginPage(driver);
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        loginPage.clickLogin();
 
-        // Get actual text
-        String actualText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/h2"))).getText();
+        String actualText = loginPage.getLoginHeaderText();
 
-        // Expected text
-        String expectedText = "Login to your account";
-        Assert.assertEquals(actualText, expectedText, "Login page did not load correctly");
+        Assert.assertEquals(actualText, "Login to your account");
 
-        // 4. Close browser
         driver.quit();
     }
 }
